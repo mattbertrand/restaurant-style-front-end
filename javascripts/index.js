7931eda15729f2a5cc4d6eb09a9cc04c205e36bd
github username <github email address>
2021-03-11 14:43:57 -0800

    added formTemplate to index.js

---
+++ javascripts/index.js
@@ -0,0 +1,24 @@
+function main() {
+    document.getElementById("main")
+}
+
+function resetMain() {
+    main().innerHTML = ""
+}
+
+function formTemplate() {
+    return `
+    <h3>Add A Restaurant</h3>
+        <form id="form">
+            <div class="input-field">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="input-field">
+                <label for="city">City</label>
+                <input type="text" name="city" id="city">
+            </div>
+            <input type="submit" value="Add Restaurant">
+        </form>
+    `
+}

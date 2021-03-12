58305fcb56ce5ab638c787847019f844e3768d8f
github username <github email address>
2021-03-11 20:18:58 -0800

    refactored const with arrow functions

--- javascripts/globals.js
+++ javascripts/globals.js
@@ -1,30 +1,12 @@
 let restaurants = []
+
 const baseUrl = 'http://localhost:3000'
 
-function main() {
-    return document.getElementById("main")
-}
+const main = () => document.getElementById("main")
+const nameInput = () => document.getElementById("name")
+const cityInput = () => document.getElementById("city")
+const styleInput = () => document.getElementById("style")
+const form = () => document.getElementById("form")
+const formLink = () => document.getElementById("form-link")
 
-function nameInput() {
-    return document.getElementById("name")
-}
-
-function cityInput() {
-    return document.getElementById("city")
-}
-
-function styleInput() {
-    return document.getElementById("style")
-}
-
-function form() {
-    return document.getElementById("form")
-}
-
-function formLink() {
-    return document.getElementById("form-link")
-}
-
-function restaurantsLink() {
-    return document.getElementById("restaurants-link")
-}
+const restaurantsLink = () => document.getElementById("restaurants-link")

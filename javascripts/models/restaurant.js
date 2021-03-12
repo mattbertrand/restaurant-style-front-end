3c9b378bf4cf490936d0f8c133730650691ff874
github username <github email address>
2021-03-11 20:39:03 -0800

    added a restaurant class with a constructor, create, save and createFromCollection method

---
+++ javascripts/models/restaurant.js
@@ -0,0 +1,24 @@
+class Restaurant {
+    static all = []
+    
+    constructor(data) {
+        this.id = data.id
+        this.name = data.name
+        this.city = data.city
+        this.style = data.style
+    }
+
+    static create(data) {
+        let restaurant = new Restaurant(data)
+        restaurant.save()
+        return restaurant
+    }
+
+    save() {
+        Restaurant.all.push(this)
+    }
+
+    static createFromCollection(collection) {
+        collection.forEach(attr => Restaurant.create(attr))
+    }
+}

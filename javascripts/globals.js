2a8b84898c8458886ec375d383907d91a0492a25
github username <github email address>
2021-03-11 20:14:32 -0800

    added a global file to hold the global variables and node getters

---
+++ javascripts/globals.js
@@ -0,0 +1,30 @@
+let restaurants = []
+const baseUrl = 'http://localhost:3000'
+
+function main() {
+    return document.getElementById("main")
+}
+
+function nameInput() {
+    return document.getElementById("name")
+}
+
+function cityInput() {
+    return document.getElementById("city")
+}
+
+function styleInput() {
+    return document.getElementById("style")
+}
+
+function form() {
+    return document.getElementById("form")
+}
+
+function formLink() {
+    return document.getElementById("form-link")
+}
+
+function restaurantsLink() {
+    return document.getElementById("restaurants-link")
+}
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,33 +1,4 @@
-let restaurants = []
-const baseUrl = 'http://localhost:3000'
 
-function main() {
-    return document.getElementById("main")
-}
-
-function nameInput() {
-    return document.getElementById("name")
-}
-
-function cityInput() {
-    return document.getElementById("city")
-}
-
-function styleInput() {
-    return document.getElementById("style")
-}
-
-function form() {
-    return document.getElementById("form")
-}
-
-function formLink() {
-    return document.getElementById("form-link")
-}
-
-function restaurantsLink() {
-    return document.getElementById("restaurants-link")
-}
 
 async function getRestaurants() {
 

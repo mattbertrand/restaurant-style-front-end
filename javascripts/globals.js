98ef715d640ab11ccadf7a478bb64e12a373c90e
github username <github email address>
2021-03-12 11:15:30 -0800

    added baseUrl to Api

--- javascripts/globals.js
+++ javascripts/globals.js
@@ -1,10 +1,7 @@
-const baseUrl = 'http://localhost:3000'
-
 const main = () => document.getElementById("main")
 const nameInput = () => document.getElementById("name")
 const cityInput = () => document.getElementById("city")
 const styleInput = () => document.getElementById("style")
 const form = () => document.getElementById("form")
 const formLink = () => document.getElementById("form-link")
-
 const restaurantsLink = () => document.getElementById("restaurants-link")
